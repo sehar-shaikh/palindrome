aceb3301202ad2410170fe535e5abc5a0a16b8a6
Sehar <Sehar@DESKTOP-F5C6FMG>
2023-03-28 20:20:42 +0500

    Add project files.

---
+++ palindrome/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "";
+            Console.WriteLine("Enter a string:");
+            str = Console.ReadLine();
+
+            char[] arr = str.ToCharArray();
+            Array.Reverse(arr);
+            string str_reserve = new string(arr);
+
+           if(str.ToLower().Equals(str_reserve.ToLower()))
+            {
+                Console.WriteLine("its a palindrome string");
+            }
+           else
+            {
+                Console.WriteLine("its not palindrome stirng");
+            }
+
+            Console.ReadLine();
+            
+
+        }
+    }
+}
